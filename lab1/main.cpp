3041270b98f7aa2faae0b2bae893c9e8dba74b2e
Artur12555 <77107684+Artur12555@users.noreply.github.com>
2022-10-12 14:24:40 +0200

    v1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <cmath>
+#include <functional>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+using namespace std;
+using funkcja = function<float(int x, int y)>;
+
+void wypiszWartosci(const funkcja& func, int x, int y=0){
+    cout << func(x, y) << endl;
+}
+// argc ilosc argumentów, **argv podwójny wskaznik na tablice + lista przekazywanych argumentów
+
+int main(int argc, char **argv) {
+    vector<string> lista(argv, argc + argv);//vector wskazuje na pierwsze ostatnie miejsce w tablicy
+    map<string, funkcja> opcje; //mapa opcje klucz jako string, value funkcja, opcje zwraca float
+    opcje["add"] = [](int x, int y) { return x + y; };
+
+    opcje["mod"] = [](int x, int y) { return x % y; };
+
+    opcje["sin"] = [](int x, int y=0) { return sin(x); };
+    try {
+        wypiszWartosci(opcje[lista.at(1)], stoi(lista.at(2)), stoi(lista.at(3)));
+    } catch (bad_function_call bfc) {
+        cout << "Opcje: add, mod, sin" << endl;
+    }
+
     return 0;
 }
